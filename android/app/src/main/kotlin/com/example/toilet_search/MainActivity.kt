605a0265c0726227ce50174e151ce5314b0bdfbd
Yuki0316 <chatoranta@gmail.com>
2023-10-23 11:28:10 +0900

    gitignore変更

--- android/app/src/main/kotlin/com/example/toilet_search/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.toilet_search
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/const.dart
+++
@@ -1,3 +0,0 @@
-class Const {
-  static String apiKey = 'AIzaSyDigGrt_J_X9mjPTxBk_bFxhD4yxuZtksA';
-}
